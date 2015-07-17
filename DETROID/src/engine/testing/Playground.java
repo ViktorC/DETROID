0cf4d33a8002b104ecc6d868f2c9aa31a6805d60
ViktorC <viktor.csomor@gmail.com>
2015-07-17 18:17:34 +0200

    -

--- DETROID/src/engine/testing/Playground.java
+++ DETROID/src/engine/testing/Playground.java
@@ -8,7 +8,7 @@ import util.*;
 public class Playground {
 
 	public static void main(String[] args) {
-		Board b = new Board("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+		Board b = new Board();
 		b.printOffsetBoardToConsole();
 		long start = System.currentTimeMillis();
 		Move.printMovesToConsole(b.generateMoves());
--- DETROID/src/util/IntList.java
+++ DETROID/src/util/IntList.java
@@ -31,10 +31,13 @@ public abstract class IntList {
 	}
 	/**The addition method that differs for different types of list data structures thus needs to be implemented by subclasses.*/
 	public abstract void add(int data);
-	/**Returns whether the iterator has more nodes to process or has already reached the end of the list.*/
+	/**Returns whether the iterator has more nodes to process or has already reached the end of the list. Once the iterator has no more nodes no to
+	 * process, the method will return 'false' and the iterator will be reset.*/
 	public boolean hasNext() {
-		if (this.iterator == null)
+		if (this.iterator == null) {
+			this.reset();
 			return false;
+		}
 		return true;
 	}
 	/**Returns the data held in the iterator and increments the iterator.*/
