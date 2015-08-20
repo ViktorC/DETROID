e54374bd9e1b5aca2adf27576e87bf7f585c69da
ViktorC <viktor.csomor@gmail.com>
2015-08-20 02:43:46 +0200

    Denser magic tables.

--- DETROID/src/engine/Playground.java
+++ DETROID/src/engine/Playground.java
@@ -4,9 +4,9 @@ package engine;
 public class Playground {
 
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
 		Position p = new Position();
-		System.out.println(p.perft(5));
+		long start = System.currentTimeMillis();
+		System.out.println(p.perft(6));
 		long end = System.currentTimeMillis();
 		System.out.println(end - start);
 	}
