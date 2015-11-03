91c1f989da3b70f7e1b89f346a694d7f070f5e14
ViktorC <viktor.csomor@gmail.com>
2015-11-03 16:42:45 +0100

    -

--- DETROID/src/engine/Playground.java
+++ DETROID/src/engine/Playground.java
@@ -3,7 +3,7 @@ package engine;
 public class Playground {
 
 	public static void main(String[] args) {
-		Position p = new Position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+		Position p = new Position();
 		long start = System.currentTimeMillis();
 		Search s = Search.getInstance(p);
 		s.run();
