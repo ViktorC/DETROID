4bb90812511248528c11d6b7ce15b942c612c675
ViktorC <viktor.csomor@gmail.com>
2015-11-05 20:08:52 +0100

    New exception for illegal interface commands.

--- DETROID/src/util/IllegalCommandArgumentException.java
+++ DETROID/src/util/IllegalCommandArgumentException.java
@@ -1,5 +1,26 @@
 package util;
 
+/**A runtime exception sub-classing {@link #java.lang.IllegalArgumentException IllegalArgumentException} for when the argument(s) following a text
+ * or any kind of command are invalid as in they are not accepted by the command interpreter.
+ * 
+ * @author Viktor
+ *
+ */
 public class IllegalCommandArgumentException extends IllegalArgumentException {
 	
+	/*Auto-generated serial code.*/
+	private static final long serialVersionUID = 8087521722058688364L;
+
+	public IllegalCommandArgumentException() {
+		
+	}
+	public IllegalCommandArgumentException(String message) {
+		super(message);
+	}
+	public IllegalCommandArgumentException(Throwable cause) {
+		super(cause);
+	}
+	public IllegalCommandArgumentException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
