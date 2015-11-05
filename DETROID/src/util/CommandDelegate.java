dc46fc189c406ef93de647e3a3f69a18c9156d77
ViktorC <viktor.csomor@gmail.com>
2015-11-05 16:04:20 +0100

    Modifications to CommandDelegate, Procedure interface deleted, IllegalCommandArgumentexception class created.

--- DETROID/src/util/CommandDelegate.java
+++ DETROID/src/util/CommandDelegate.java
@@ -1,6 +1,6 @@
 package util;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 /**A generic class for command handling. The Command object itself has its inherent functionality and execution conditions assigned to it on
  * initialization. Commands can be executed by calling {@link #execute(T) execute} with the command as the argument.
@@ -12,16 +12,16 @@ import java.util.function.Predicate;
 public class CommandDelegate<T> {
 
 	private Predicate<T> predicate;	// The conditions for execution.
-	private Procedure procedure;	// The functionality of the command.
+	private Consumer<T> consumer;	// The functionality of the command.
 	
 	/**Initializes the handler by assigning the execution conditions and command functionalities to it.
 	 * 
 	 * @param predicate The conditions for execution.
-	 * @param procedure The functionality of the command.
+	 * @param function The functionality of the command.
 	 */
-	public CommandDelegate(Predicate<T> predicate, Procedure procedure) {
+	public CommandDelegate(Predicate<T> predicate, Consumer<T> consumer) {
 		this.predicate = predicate;
-		this.procedure = procedure;
+		this.consumer = consumer;
 	}
 	/**Tries to execute the argument command; if it fits the conditions, it gets executed and the method returns true, else it returns false.
 	 * 
@@ -30,9 +30,7 @@ public class CommandDelegate<T> {
 	 */
 	public boolean execute(T command) {
 		if (predicate.test(command)) {
-			procedure.execute();
-			return true;
+			consumer.accept(command);
 		}
-		return false;
 	}
 }
