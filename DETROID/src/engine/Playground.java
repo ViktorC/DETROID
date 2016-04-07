9820a39b5f11a18a6179986336378d3c67ed0028
ViktorC <viktor.csomor@gmail.com>
2016-04-07 19:58:42 +0200

    -

--- DETROID/src/engine/Playground.java
+++ DETROID/src/engine/Playground.java
@@ -1,9 +1,5 @@
 package engine;
 
-import java.util.Observable;
-import java.util.Observer;
-
-import engine.Search.Results;
 import util.HashTable;
 
 public class Playground {
@@ -12,26 +8,17 @@ public class Playground {
 	final static String tP2 = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -";
 	final static String tP3 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
 	
-	public static class PVO implements Observer {
-
-		@Override
-		public void update(Observable arg0, Object arg1) {
-			System.out.println(arg0);
-		}
-		
-	}
-	
 	public static void main(String[] args) {
-		Position p = new Position(tP2);
+		Position p = new Position(tP1);
 		RelativeHistoryTable hT = new RelativeHistoryTable();
 		HashTable<TTEntry> tT = new HashTable<>(256);
 		HashTable<ETEntry> eT = new HashTable<>(192);
 		HashTable<PTEntry> pT = new HashTable<>(16);
 		Search s = new Search(p, 0, 0, 10, 0, null, hT, (byte) 0, tT, eT, pT, 4);
 		long start = System.currentTimeMillis();
-		s.getResults().addObserver(new PVO());
 		s.run();
 		long end = System.currentTimeMillis();
+		System.out.println(s.getResults());
 		System.out.println(end - start);
 	}
 }
