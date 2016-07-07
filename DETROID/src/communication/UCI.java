86cb003ac8b79751adbc4c962aaa0bcdac2f84d8
ViktorC <viktor.csomor@gmail.com>
2016-07-07 23:59:01 +0200

    -

--- DETROID/src/communication/UCI.java
+++ DETROID/src/communication/UCI.java
@@ -10,17 +10,82 @@ import util.KeyValuePair;
  */
 public interface UCI {
 
-	String uci();
-	String id();
+	public final static String[] SEARCH_ARGS = new String[] { "searchmoves", "ponder", "wtime", "bTime", "winc", "binc", "movestogo", "depth",
+		"nodes", "mate", "movetime", "infinite" };
+	
+	/**
+	 * Tells the engine to switch to UCI mode.
+	 * 
+	 * @return Whether the engine successfully switched to UCI mode.
+	 */
+	boolean uci();
+	/**
+	 * The name of the engine.
+	 * 
+	 * @return
+	 */
+	String getName();
+	/**
+	 * The name of the author of the engine.
+	 * 
+	 * @return
+	 */
+	String getAuthor();
+	/**
+	 * Sets whether the engine should run in debug mode. In debug mode, the engine can (and is expected to) send detailed info strings
+	 * such as search statistics.
+	 * 
+	 * @param on
+	 */
 	void debug(boolean on);
-	String isReady();
+	/**
+	 * Asks the engine whether it is done doing what the GUI commanded it to do and waits for the response.
+	 * 
+	 * @return
+	 */
+	boolean isReady();
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
 	KeyValuePair<String, String>[] options();
+	/**
+	 * Sets an option defined by the engine to the specified value.
+	 * 
+	 * @param option
+	 */
 	void setOption(KeyValuePair<String, String> option);
-	String register();
+	/**
+	 * Resets the game to a new instance.
+	 */
 	void uciNewGame();
+	/**
+	 * Sends the current position to the engine.
+	 * 
+	 * @param fen
+	 */
 	void position(String fen);
-	String go(KeyValuePair<String, String>[] params);
-	void stop();
+	/**
+	 * Asks the engine to start searching the current position according to the specified parameters.
+	 * 
+	 * @param params
+	 * @return The best move found in long algebraic notation.
+	 */
+	String go(KeyValuePair<String, Object>[] params);
+	/**
+	 * Asks the engine to stop searching and return the best move found up until that point.
+	 * 
+	 * @return The best move found in long algebraic notation.
+	 */
+	String stop();
+	/**
+	 * Tells the engine that ponder move fed to the search was actually made, so the engine should search further but not in ponder mode
+	 * anymore.
+	 */
 	void ponderHit();
+	/**
+	 * Asks the engine to shut down.
+	 */
 	void quit();
 }
--- DETROID/src/engine/Engine.java
+++ DETROID/src/engine/Engine.java
@@ -1,8 +1,5 @@
 package engine;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.Observer;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
@@ -14,19 +11,20 @@ import engine.Book.SelectionModel;
 import engine.Search.Results;
 import util.*;
 
-public class Engine implements UCI {
+public class Engine /*implements UCI*/ {
 
-	private final static InputStream DEFAULT_INPUT_STREAM = System.in;
-	private final static OutputStream DEFAULT_OUTPUT_STREAM = System.out;
+	private final static Engine INSTANCE = new Engine();
+	
+	private final static String NAME = "DETROID";
+	private final static String AUTHOR = "Viktor Csomor";
 	private final static int DEFAULT_HASH_SIZE = 128;
 	
-	private final static Engine INSTANCE = new Engine();
+	private boolean debug;
 	
 	private ExecutorService background;
 	private List<Future<?>> backgroundTasks;
 	
 	private Scanner in;
-	private PrintStream out;
 	
 	private List<Observer> observers;
 	
@@ -48,9 +46,8 @@ public class Engine implements UCI {
 		background = Executors.newCachedThreadPool();
 		backgroundTasks = new Queue<>();
 		observers = new Queue<>();
-		setInputStream(DEFAULT_INPUT_STREAM);
-		setOutputStream(DEFAULT_OUTPUT_STREAM);
 		setHashSize(DEFAULT_HASH_SIZE);
+		in = new Scanner(System.in);
 		params = new Parameters();
 		hT = new RelativeHistoryTable(params);
 		tT = new HashTable<>(maxHashMemory/2, TTEntry.SIZE);
@@ -61,16 +58,6 @@ public class Engine implements UCI {
 	}
 	public Engine getInstance() {
 		return INSTANCE;
-	}
-	public void setInputStream(InputStream in) {
-		if (this.in != null)
-			this.in.close();
-		this.in = new Scanner(in);
-	}
-	public void setOutputStream(OutputStream out) {
-		if (this.out != null)
-			this.out.close();
-		this.out = new PrintStream(out);
 	}
 	public void subscribe(Observer obs) {
 		observers.add(obs);
@@ -117,69 +104,5 @@ public class Engine implements UCI {
 		}
 		return res.getPvLine() == null ? null : res.getPvLine().getHead();
 	}
-	@Override
-	public String uci() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String id() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void debug(boolean on) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public String isReady() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public KeyValuePair<String, String>[] options() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void setOption(KeyValuePair<String, String> option) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public String register() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void uciNewGame() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void position(String fen) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public String go(KeyValuePair<String, String>[] params) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void ponderHit() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void quit() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
