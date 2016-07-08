2f5c0ac2fd5ed4a73d3552d395d7bde681417c50
ViktorC <viktor.csomor@gmail.com>
2016-07-08 20:22:53 +0200

    -

--- DETROID/src/communication/UCI.java
+++ DETROID/src/communication/UCI.java
@@ -10,8 +10,89 @@ import util.KeyValuePair;
  */
 public interface UCI {
 
-	public final static String[] SEARCH_ARGS = new String[] { "searchmoves", "ponder", "wtime", "bTime", "winc", "binc", "movestogo", "depth",
-		"nodes", "mate", "movetime", "infinite" };
+	/**
+	 * The possible parameters in a 'go' command.
+	 * 
+	 * @author Viktor
+	 *
+	 */
+	public enum SearchAttributes {
+		searchmoves,
+		ponder,
+		wtime,
+		btime,
+		winc,
+		binc,
+		movestogo,
+		depth,
+		nodes,
+		mate,
+		movetime,
+		infinite
+	}
+	/**
+	 * The possible parameters sent to the GUI in info strings when in debug mode.
+	 * 
+	 * @author Viktor
+	 *
+	 */
+	public enum InfoAttributes {
+		depth,
+		seldepth,
+		time,
+		nodes,
+		pv,
+		multipv,
+		score,
+		currmove,
+		currmovenumber,
+		hashfull,
+		nps,
+		tbhits,
+		cpuload,
+		string,
+		refutation,
+		currline
+	}
+	/**
+	 * The possible types of the score attribute from the info attributes.
+	 * 
+	 * @author Viktor
+	 *
+	 */
+	public enum InfoScoreTypes {
+		cp,
+		mate,
+		lowerbound,
+		upperbound
+	}
+	/**
+	 * The attributes that can and have to be specified (according to the type of the option) in the 'option' command.
+	 * 
+	 * @author Viktor
+	 *
+	 */
+	public enum OptionAttributes {
+		name,
+		type,
+		default_,
+		min,
+		max,
+		var
+	}
+	/**
+	 * The different values the 'type' attribute from the option attributes can take on.
+	 * 
+	 * @author Viktor
+	 *
+	 */
+	public enum OptionTypes {
+		check,
+		spin,
+		combo,
+		button,
+		string
+	}
 	
 	/**
 	 * Tells the engine to switch to UCI mode.
@@ -33,7 +114,7 @@ public interface UCI {
 	String getAuthor();
 	/**
 	 * Sets whether the engine should run in debug mode. In debug mode, the engine can (and is expected to) send detailed info strings
-	 * such as search statistics.
+	 * such as search statistics to the GUI.
 	 * 
 	 * @param on
 	 */
@@ -45,17 +126,19 @@ public interface UCI {
 	 */
 	boolean isReady();
 	/**
-	 * 
+	 * Returns the options the engine offers. Each option is defined by an array of KeyValuePairs of varying numbers of elements specifying the
+	 * name, type, default value, etc. of the option.
 	 * 
 	 * @return
 	 */
-	KeyValuePair<String, String>[] options();
+	KeyValuePair<String, ?>[][] options();
 	/**
 	 * Sets an option defined by the engine to the specified value.
 	 * 
-	 * @param option
+	 * @param optionName
+	 * @param value
 	 */
-	void setOption(KeyValuePair<String, String> option);
+	void setOption(String optionName, Object value);
 	/**
 	 * Resets the game to a new instance.
 	 */
@@ -72,7 +155,7 @@ public interface UCI {
 	 * @param params
 	 * @return The best move found in long algebraic notation.
 	 */
-	String go(KeyValuePair<String, Object>[] params);
+	String go(KeyValuePair<String, ?>[] params);
 	/**
 	 * Asks the engine to stop searching and return the best move found up until that point.
 	 * 
