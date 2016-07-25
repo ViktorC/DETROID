106e69de521a9b7b1eb2e4514112c61dd383d73c
ViktorC <viktor.csomor@gmail.com>
2016-07-25 17:12:07 +0200

    -

--- DETROID/src/engine/Detroid.java
+++ DETROID/src/engine/Detroid.java
@@ -71,7 +71,8 @@ public class Detroid implements Engine, Observer {
 		blackIncrement = blackIncrement == null ? 0 : blackIncrement;
 		if (movesToGo == null) {
 			phaseScore = new Evaluator(params, eT, pT, gen).phaseScore(game.getPosition());
-			movesToGo = Math.max(10, Game.AVG_MOVES_PER_GAME - Game.AVG_MOVES_PER_GAME*phaseScore/params.GAME_PHASE_END_GAME_UPPER);
+			movesToGo = Math.max(params.MOVES_TO_GO_SAFETY_MARGIN,
+					Game.AVG_MOVES_PER_GAME - Game.AVG_MOVES_PER_GAME*phaseScore/params.GAME_PHASE_END_GAME_UPPER);
 			if (debug) debugInfo.set("Search time data\n" +
 					"Phase score - " + phaseScore + "/256\n" +
 					"Expected number of moves left until end - " + movesToGo);
